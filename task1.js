aba07bd4fd7650678f9aa8573f3c964e7a609c91
sharma-22-dev <nagarajsharma702@gmail.com>
2026-08-27 13:05:02 +0530

    Initial commit

---
+++ task1.js
@@ -0,0 +1,19 @@
+let studentName = "tom";
+let studentage = 20;
+let studentmark = 95;
+
+console.log(studentName);
+console.log(studentage);
+console.log(studentmark);
+
+let name = prompt("Enter your name:");
+alert(name);
+
+let age = prompt("Enter your age:");
+console.log(age);
+
+let result = confirm("Do you want to continue?");
+console.log(result);
+
+let name1 = prompt("Enter your name:");
+document.writeln(name);
